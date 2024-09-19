bd63dddae9ed931b64ec6c0edc1ea74570d2f65a
thedoorknocks <nathanl60@nycstudents.net>
2024-09-19 16:05:04 +0000

    hi

--- Feeder.java
+++ Feeder.java
@@ -23,5 +23,8 @@ public class Feeder {
     {
         return 0;
     }
-
+    public int getCurrentFood()
+    {
+        return currentFood;
+    }   
 }
---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Feeder f = new Feeder();
+        System.out.println(f);
+        System.out.println(f.currentFood);
+    }
+    
+//System.out.println(f.currentFood);
+
+}
